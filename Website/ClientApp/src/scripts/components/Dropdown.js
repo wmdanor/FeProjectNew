9b152e06108ff5e63b291b6ad8650553be0ec913
wmdanor_PC <wmdanor@gmail.com>
2021-05-21 23:09:30 +0300

    Initial commit

---
+++ Website/ClientApp/src/scripts/components/Dropdown.js
@@ -0,0 +1,50 @@
+﻿import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import * as React from 'react';
+
+function DropdownToggle(props) {
+  return <span className="dropdown-toggle">{props.children}</span>;
+}
+
+DropdownToggle.propTypes = {
+  children: PropTypes.node,
+};
+
+function DropdownItem(props) {
+  return <li className="dropdown-item">{props.children}</li>;
+}
+
+DropdownItem.propTypes = {
+  children: PropTypes.node,
+};
+
+function DropdownLink(props) {
+  return (
+    <DropdownItem>
+      <Link to={props.to}>{props.children}</Link>
+    </DropdownItem>
+  );
+}
+
+DropdownLink.propTypes = {
+  children: PropTypes.node,
+  to: PropTypes.string.isRequired,
+};
+
+function DropdownList(props) {
+  return <ul className="dropdown-list">{props.children}</ul>;
+}
+
+DropdownList.propTypes = {
+  children: PropTypes.node,
+};
+
+function Dropdown(props) {
+  return <div className="dropdown">{props.children}</div>;
+}
+
+Dropdown.propTypes = {
+  children: PropTypes.node,
+};
+
+export { DropdownToggle, DropdownItem, DropdownLink, DropdownList, Dropdown };
---
+++ Website/ClientApp/src/scripts/site.js
@@ -0,0 +1,112 @@
+﻿import 'normalize.css';
+import '../styles/style.scss';
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import * as ReactRouterDOM from 'react-router-dom';
+
+import { NavbarLink, Navbar, NavbarDropdown } from './components/Navbar';
+import {
+  DropdownToggle,
+  DropdownList,
+  DropdownItem,
+} from './components/Dropdown';
+// import { GridList } from './components/GridList';
+
+const Router = ReactRouterDOM.BrowserRouter;
+const { Route } = ReactRouterDOM;
+const { Switch } = ReactRouterDOM;
+
+function About() {
+  return <h2>О сайте</h2>;
+}
+
+function NotFound() {
+  return <h2>Ресурс не найден</h2>;
+}
+
+// const products = [
+//   {
+//     id: 1,
+//     name: 'Game',
+//     price: 10,
+//   },
+//   {
+//     id: 2,
+//     name: 'Cock',
+//     price: 5,
+//   },
+//   {
+//     id: 3,
+//     name: 'Anus',
+//     price: 30,
+//   },
+//   {
+//     id: 4,
+//     name: 'Phone',
+//     price: 50,
+//   },
+// ];
+
+function Main() {
+  // return <GridList data={products} />;
+  return <div>Main</div>;
+}
+
+function AppNavbar() {
+  return (
+    <Navbar>
+      <NavbarLink to="/">Home</NavbarLink>
+      <NavbarLink to="/about">About</NavbarLink>
+      <NavbarDropdown>
+        <DropdownToggle>Dropdown</DropdownToggle>
+        <DropdownList>
+          <DropdownItem>One</DropdownItem>
+          <DropdownItem>Two</DropdownItem>
+        </DropdownList>
+      </NavbarDropdown>
+    </Navbar>
+  );
+}
+
+function Header() {
+  return (
+    <>
+      <header>
+        <iframe
+          width="560"
+          height="315"
+          src="https://www.youtube.com/embed/AE7QT5anSHY"
+          title="YouTube video player"
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+        />
+      </header>
+      <AppNavbar />
+    </>
+  );
+}
+
+function Footer() {
+  return (
+    <footer>
+      <p>Footer</p>
+    </footer>
+  );
+}
+
+function App() {
+  return (
+    <Router>
+      <Header />
+      <Switch>
+        <Route exact path="/" component={Main} />
+        <Route path="/about" component={About} />
+        <Route component={NotFound} />
+      </Switch>
+      <Footer />
+    </Router>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
